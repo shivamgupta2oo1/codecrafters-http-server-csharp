d03a485029fa4aa1ccafbf15e8514d6b4c840580
shivamgupta <shivamgupta08032001@gmail.com>
2024-05-14 10:23:40 +0530

    pass 2st stage

--- src/Server.cs
+++ src/Server.cs
@@ -6,3 +6,16 @@ Console.WriteLine("Logs from your program will appear here!");
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 server.AcceptSocket(); // wait for client
+
+using TcpClient client = await server.AcceptTcpClientAsync();
+await using NetworkStream stream = client.GetStream();
+// Read incoming data
+byte[] requestBuffer = new byte[1024];
+int bytesRead = await stream.ReadAsync(requestBuffer, 0, requestBuffer.Length);
+string request =
+    System.Text.Encoding.UTF8.GetString(requestBuffer, 0, bytesRead);
+Console.WriteLine($"Received request: {request}");
+// Prepare and send response
+string response = "HTTP/1.1 200 OK\r\n\r\n";
+byte[] responseBuffer = System.Text.Encoding.UTF8.GetBytes(response);
+stream.Write(responseBuffer, 0, responseBuffer.Length);
